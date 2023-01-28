6cc3e5a60fc9ccab9392fc84317a7a2d07450d08
CookieHunteRRR <cookievip2014@gmail.com>
2023-01-29 02:03:34 +0700

    Обновлен BuildingConfig, добавлен RequiredResourceProperties для списка требуемых для постройки ресурсов

--- Assets/Code/Configs/BuildingConfig.cs
+++ Assets/Code/Configs/BuildingConfig.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
 
 namespace Code.Configs
 {
@@ -8,6 +10,13 @@ namespace Code.Configs
         public Transform Prefab;
         public Transform Icon;
         [SerializeField] private BuildingsType _type;
+        [SerializeField] private string _name;
+        [SerializeField] private string _description;
+        [SerializeField] private List<RequiredResourceProperties> _requiredResources;
+        [SerializeField] private int _minWorkerAmount; // Минимальное количество персонала, необходимое для работы здания
+        [SerializeField] private int _maxWorkerAmount; // Максимальное количество персонала, необходимое для работы здания
+        [SerializeField] private int _energyPerPerson; // Потребляемая энергия одним работником
+        [SerializeField] private int _energyForUpkeep; // Потребляемая энергия вне зависимости от работников
 
         public BuildingsType Type => _type;
     }
---
+++ Assets/Code/Configs/RequiredResourceProperties.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Configs
+{
+    // Используется для указания типа ресурса и количества этого ресурса необходимого для постройки здания
+    [System.Serializable]
+    internal class RequiredResourceProperties
+    {
+        public ResourcesType Type;
+        public int Amount;
+    }
+}
